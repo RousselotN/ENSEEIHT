251a50d9c192774b1f94017e3d594be4221132e2
Nathan Rousselot <nathan.rousselot@efrei.net>
2020-09-25 11:00:40 +0200

    Add files via upload

---
+++ S5/BE1/Exo9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* EXO9 BE1 */
+/* Prototype */
+
+
+int main(){
+    int a, b, x;
+    printf("Entrez a: ");
+    scanf(" %d", &a);
+    printf("\nEntrez b: ");
+    scanf(" %d", &b);
+    printf("\nEntrez x: ");
+    scanf(" %d", &x);
+    printf("\nLe resultat de ax+b est %d", findy(a,x,b));
+    return 0;
+}
+
+int findy(int a, int x, int b){
+    return a * x + b;
+}
